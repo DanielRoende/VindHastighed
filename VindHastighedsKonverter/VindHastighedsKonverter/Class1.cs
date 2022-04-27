1fbfdf34982009ef74a5c6256fc107a79ccae29f
DHR <82029094+DanielRoende@users.noreply.github.com>
2022-04-27 13:58:14 +0200

    Prog
    Option for change is doing this with an API instead

---
+++ VindHastighedsKonverter/VindHastighedsKonverter/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace VindHastighedsKonverter
+{
+    internal class Methods
+    {
+
+        public void Ms_Km()
+        {
+            Console.WriteLine("Input = m/sec");
+            int ms = Convert.ToInt32(Console.ReadLine());
+            var ConvertedMs = (ms * 18) / 5;
+            Console.WriteLine(ConvertedMs);
+        }
+        public void Mps_K()
+        {
+            Console.WriteLine("Input = Mps");
+            int mph = Convert.ToInt32(Console.ReadLine());
+            var convertedMph = (mph * 0.868976);
+            Console.WriteLine(convertedMph);
+        }
+
+    }
+}
